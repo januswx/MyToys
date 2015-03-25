8ebefd7586f8bb90f318cdf0a441f5110962b2ad
januswx <janusyzh@outlook.com>
2015-03-25 10:40:32 +0800

    单词反转

---
+++ MyToys/ReverseSentence.Test/ReverseHelperTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseSentence.Test
+{
+    [TestClass]
+    public class ReverseHelperTest
+    {
+        [TestMethod]
+        public void ReverseWordsTest()
+        {
+            string input = "how, are, you!";
+            string output = ReverseHelper.ReverseWords(input);
+
+            Assert.AreEqual("!you ,are ,how", output);
+        }
+    }
+}
