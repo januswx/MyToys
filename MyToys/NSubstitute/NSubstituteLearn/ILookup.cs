5342ee2168ef14e2fc056b116520da0e05dd43eb
januswx <janusyzh@outlook.com>
2016-05-18 11:52:20 +0800

    添加 16 设置out和ref参数

---
+++ MyToys/NSubstitute/NSubstituteLearn/ILookup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSubstituteLearn
+{
+    public interface ILookup
+    {
+        bool TryLookup(string key, out string value);
+    }
+}
---
+++ MyToys/NSubstitute/NSubstituteLearn/NUnitTest16.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Common;
+using NSubstitute;
+using NSubstitute.Exceptions;
+using System.Collections.Generic;
+
+namespace NSubstituteLearn
+{
+    /// <summary>
+    /// 设置out和ref参数
+    /// </summary>
+    [TestFixture]
+    public class NUnitTest16
+    {
+        /// <summary>
+        /// 我们可以配置其返回值，并设置第二个参数的输出
+        /// </summary>
+        [Test]
+        public void Test_SetOutRefArgs_SetOutArg()
+        {
+            // Arrange
+            var value = "";
+            var lookup = Substitute.For<ILookup>();
+            lookup
+              .TryLookup("hello", out value)
+              .Returns(x =>
+              {
+                  x[1] = "world!";
+                  return true;
+              });
+
+            // Act
+            var result = lookup.TryLookup("hello", out value);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(value, "world!");
+        }
+    }
+}
